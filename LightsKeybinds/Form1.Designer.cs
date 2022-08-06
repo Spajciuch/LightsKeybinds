a129dc9d384b299959ff697a57051ed92db8f9b4
Spyte <spyte@pm.me>
2022-08-06 20:51:37 +0200

    Dodaj pliki projektów.

---
+++ LightsKeybinds/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace LightsKeybinds
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "UP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(50, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 60);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "EVERYTHING";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(50, 144);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 60);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "DOWN";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Lights";
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 230);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ LightsKeybinds/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+
+namespace LightsKeybinds
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        const int MYACTION_HOTKEY_ID = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "";
+
+            this.ShowInTaskbar = false;
+            this.WindowState = FormWindowState.Minimized;
+
+            int upKeyID = 1;
+            int allKeyID = 2;
+            int downKeyID = 3;
+
+            int HotKeyUP = (int)Keys.F15;
+            Boolean keyUpRegistered = RegisterHotKey(
+                this.Handle, upKeyID, 0x0000, HotKeyUP
+            );
+
+            int HotKeyALL = (int)Keys.F16;
+            Boolean keyAllRegistered = RegisterHotKey(
+                this.Handle, allKeyID, 0x0000, HotKeyALL
+            );
+
+            int HotKeyDOWN = (int)Keys.F17;
+            Boolean keyDownRegistered = RegisterHotKey(
+                this.Handle, downKeyID, 0x0000, HotKeyDOWN
+            );    
+
+            if (keyUpRegistered)
+            {
+                Console.WriteLine("Global Hotkey F15 was succesfully registered");
+            }
+            else
+            {
+                Console.WriteLine("Global Hotkey F15 couldn't be registered !");
+            }
+
+            if (keyAllRegistered)
+            {
+                Console.WriteLine("Global Hotkey F16 was succesfully registered");
+            }
+            else
+            {
+                Console.WriteLine("Global Hotkey F16 couldn't be registered !");
+            }
+
+            if (keyDownRegistered)
+            {
+                Console.WriteLine("Global Hotkey F17 was succesfully registered");
+            }
+            else
+            {
+                Console.WriteLine("Global Hotkey F17 couldn't be registered !");
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sendMessage("UP");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sendMessage("ALL");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sendMessage("DOWN");
+        }
+
+        public void sendMessage(string message)
+        {
+            var client = new TcpClient();
+            client.Connect("192.168.0.64", 3001);
+
+            NetworkStream networkStream = client.GetStream();
+            networkStream.ReadTimeout = 2000;
+
+            byte[] bytes = Encoding.UTF8.GetBytes(message);
+
+            networkStream.Write(bytes, 0, bytes.Length);
+
+            client.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon.Visible = true;
+            }
+        }
+
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = false;
+        }
+        protected override void WndProc(ref Message m)
+        { 
+            if (m.Msg == 0x0312)
+            {
+                int id = m.WParam.ToInt32();
+
+                switch(id)
+                {
+                    case 1:
+                        sendMessage("UP");
+                        break;
+                    case 2:
+                        sendMessage("ALL");
+                        break;
+                    case 3:
+                        sendMessage("DOWN");
+                        break;
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+    }
+}
